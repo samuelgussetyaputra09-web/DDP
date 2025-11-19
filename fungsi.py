0064a6cffde6c63bc997f2a502b5a041a1afead4
samuelgussetyaputra09-web <samuelgussetyaputra09@gmail.com>
2025-11-19 09:59:03 +0700

    Add files via upload

---
+++ fungsi.py
@@ -0,0 +1,27 @@
+def celcius_ke_fahrenheit(celcius):
+    """Konversi suhu dari Celcius ke Fahrenheit."""
+    return celcius * 9/5 + 32
+
+print(celcius_ke_fahrenheit(0))    
+print(celcius_ke_fahrenheit(100)) 
+
+def is_genap(n):
+    """Mengembalikan True jika n genap, False jika ganjil."""
+    return n % 2 == 0
+
+print(is_genap(4))  
+print(is_genap(7))  
+
+
+def nilai(score):
+    
+    return "lulus" if score >= 75 else "gagal"
+
+print(nilai(80))  
+print(nilai(60))  
+
+def bilangan_ganjil_kurang(angka):
+    ganjil = [i for i in range (1, angka) if i % 2!=0]
+    return ganjil
+
+print(bilangan_ganjil_kurang(20))
